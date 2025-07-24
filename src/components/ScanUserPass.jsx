d42aa127c051ca1fb67478796b2bb6ab4a5c880f
MansiGohil-dev <gohilmansi523@gmail.com>
2025-07-24 20:14:07 +0530

    New Update

--- src/components/ScanUserPass.jsx
+++ src/components/ScanUserPass.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { QrScanner } from '@yudiel/react-qr-scanner';
+import QrReader from 'react-qr-reader';
 import { useNavigate } from 'react-router-dom';
 import 'webrtc-adapter';
 
